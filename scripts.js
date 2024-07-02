5072ebd034d024fbedcdfb36ccad0229182c8306
Hanna-Maryia Savitskaya <103072354+sans-et@users.noreply.github.com>
2024-07-02 20:22:36 +0300

    add: animations + adaptation

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   window.addEventListener("scroll", function () {
+//     const image = document.getElementById("IC");
+//     const imagePosition = image.getBoundingClientRect().top;
+//     const screenPosition = window.innerHeight / 1.5;
+//     if (imagePosition < screenPosition) {
+//       image.classList.add("animation");
+//     }
+//   });
+// });
+document.addEventListener("DOMContentLoaded", function () {
+  const image = document.getElementById("mainChicken");
+  image.classList.add("animate");
+});
